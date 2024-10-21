da314994e24b41428a880a20ff5c695a8e3ba6f2
MohamedMat-hana <mathanamohamed9@gmail.com>
2024-10-21 16:56:00 +0300

    sending email done

--- utils/Mailler.js
+++ utils/Mailler.js
@@ -1,41 +1,38 @@
-var _ = require('lodash');
-var nodemailer = require('nodemailer');
+var _ = require("lodash");
+var nodemailer = require("nodemailer");
 
 var config = {
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  service: 'gmail',
   auth: {
-    user: 'mathanamohamed9@gmail.com',
-    pass: '1942001Mm'
-  }
+    user: "mathanamohamed9@gmail.com",
+    pass: "umrc zdgs vkxr stua",
+  },
 };
 
 var transporter = nodemailer.createTransport(config);
 
 var defaultMail = {
-  from: 'mathanamohamed9@gmail.com',
-  text: 'test test',
+  from: "mathanamohamed9@gmail.com",
+  subject: "test email",
+  text: "test test",
 };
 
 const send = ({ to, subject, html }) => {
-  // Merge default options with the actual email details
-  const mailOptions = {
-    ...defaultMail, // Defaults like 'from'
-    to,             // Destination email
-    subject,        // Email subject
-    html            // Email HTML content
+   const mailOptions = {
+    ...defaultMail,  
+    to, 
+    subject, 
+    html,  
   };
 
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
+   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log("Error sending mail:", error);
     }
-    console.log('Mail sent:', info.response);
+    console.log("Mail sent:", info.response);
   });
 };
 
 module.exports = {
-  send
+  send,
 };
