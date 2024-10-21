64b4b929bfc3e7d76160b1b4b92645e162f14b73
MohamedMat-hana <mathanamohamed9@gmail.com>
2024-10-21 03:17:55 +0300

    edit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,64 +1,87 @@
 const User = require("../models/UserModel");
 const mailer = require("../utils/Mailler");
-const generateToken = require("../utils/generateToken");
-const crypto = require("crypto")
+const crypto = require("crypto");
+
 const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
+
     const userExists = await User.findOne({ email });
-    if (userExists && userExists.active) {
-      return res.status(400).json({
-        success: false,
-        msg: "User already exists",
-      });
-    } else if (!userExists && !userExists.active) {
-      return res.status(400).json({
-        success: false,
-        msg: "account is here but need to active",
-      });
+
+    if (userExists) {
+      if (userExists.active) {
+        return res.status(400).json({
+          success: false,
+          msg: "User already exists",
+        });
+      } else {
+        return res.status(400).json({
+          success: false,
+          msg: "Account exists but needs to be activated",
+        });
+      }
     }
 
+    // Create a new user if they don't exist
     const user = new User({
       name,
       email,
       password,
     });
 
-    crypto.randomBytes(20,function (err,buf){
-      user.activeToken=user._id+buf.toString("hex");
-
-      user.activeExpires=Date.now()+24*3600*1000;
-      var link = process.env.NODE_ENV=='development'?`http://localhost:${process.env.PORT}/api/users/active/${user.activeToken}`
-      :`${process.env.api_host}/api/users/active/${user.activeToken}`;
-      mailer.send({
-        to: req.body.email,
-        subject: "Welcome",
-        html:'please click <a href="'+link+'"> hetre </a> to active your account. '
-      });
-
-      async function createUser(req, res) {
-        const user = new User(req.body);
-        
-        try {
-          await user.save(); // Now this will work
-          res.status(200).send({ message: 'User saved successfully' });
-        } catch (err) {
-          res.status(500).send({ message: 'Error saving user' });
-        }
+    crypto.randomBytes(20, async (err, buf) => {
+      if (err) {
+        return res.status(500).json({
+          success: false,
+          msg: "Error generating activation token",
+        });
       }
-      
-      
-    })
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      msg: "server having some issues"
-    })
-    
-  }
-};
 
-module.exports={
-  registerUser
+      user.activeToken = user._id + buf.toString("hex");
+      user.activeExpires = Date.now() + 24 * 3600 * 1000;
+
+      const link =
+        process.env.NODE_ENV == "development"
+          ? `http://localhost:${process.env.PORT}/api/users/activate/${user.activeToken}`
+          : `${process.env.api_host}/api/users/activate/${user.activeToken}`;
+
+      // Send activation email
+      try {
+        await mailer.send({
+          to: email,
+          subject: "Welcome",
+          html: `Please click <a href="${link}">here</a> to activate your account.`,
+        });
+      } catch (mailError) {
+        return res.status(500).json({
+          success: false,
+          msg: "Error sending activation email",
+        });
+      }
+
+      // Save user
+      try {
+        await user.save();
+        return res.status(201).json({
+          success: true,
+          msg: "User registered. Activation email sent.",
+        });
+      } catch (saveError) {
+        return res.status(500).json({
+          success: false,
+          msg: "Error saving user",
+        });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      msg: "Server having some issues",
+    });
+  }
 }
+
+module.exports = {
+  registerUser,
+};
--- utils/Mailler.js
+++ utils/Mailler.js
@@ -1,30 +1,38 @@
 var _ = require("lodash");
 const nodemailer = require("nodemailer");
 
+// Configuration (credentials should be stored in environment variables)
 var config = {
   host: "smtp.gmail.com",
   port: 587,
+  secure: false, // Use TLS
   auth: {
-    user: "mathanamohamed9@gmail.com",
-    pass: "19456955s",
+    user: "mathanamohamed9@gmail.com", // Use environment variables
+    pass: "1942001Mm", // Use environment variables
   },
 };
 
+// Create the transporter
 var transporter = nodemailer.createTransport(config);
 
+// Default mail object (this can include default from, etc.)
 var defaultMail = {
-  from: "mathanamohamed9@gmail.com",
-  text: "testy",
+  from: "mathanamohamed9@gmail.com", // Default from address
+  text: "This is a test email",  // Default text body
 };
 
+// Send function
 const send = (to, subject, html) => {
-  mail = _.merge({ html }, defaultMail, to);
+  const mail = _.merge({}, defaultMail, { to, subject, html });
   transporter.sendMail(mail, function (error, info) {
-    if (error) return console.log(error);
-    console.log("mail sent", info.response);
+    if (error) {
+      return console.log("Error sending mail:", error);
+    }
+    console.log("Mail sent:", info.response);
   });
 };
 
+// Export the send function
 module.exports = {
   send,
 };
