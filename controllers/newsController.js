9ad1cb762245ed67beaa9b94c827bef6127a056d
MohamedMat-hana <mathanamohamed9@gmail.com>
2024-11-01 06:29:36 +0200

    apis added done

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -6,7 +6,7 @@ const addNews = async (req, res, next) => {
     console.log(req.body);
     const { title, category, author, content, addToSlider } = req.body;
 
-     if (!req.files || !req.files.newsImage || !req.files.newsImage.path) {
+    if (!req.files || !req.files.newsImage || !req.files.newsImage.path) {
       return res
         .status(400)
         .json({ success: false, message: "Image file is missing" });
@@ -21,7 +21,7 @@ const addNews = async (req, res, next) => {
       addToSlider,
       newsImage: `data:${
         req.files.newsImage.type || "image/png"
-      };base64,${base64Data}`, 
+      };base64,${base64Data}`,
       addedAt: Date.now(),
     });
 
@@ -41,6 +41,155 @@ const addNews = async (req, res, next) => {
   }
 };
 
+const getAllNews = async (req, res, next) => {
+  try {
+    const size = req.params.pageSize;
+    const pageNo = req.params.pageNo;
+    var query = {};
+
+    if (pageNo < 0 || pageNo === 0) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid Page Number, should start with 1",
+      });
+    }
+    query.skip = size * (pageNo - 1);
+    query.limit = size;
+    const newsCount = await News.find({});
+    const news = await News.find({})
+      .sort("-addedAt")
+      .populate({ path: "category", select: ["_id", "category_name"] })
+      .limit(Number(query.limit))
+      .skip(Number(query.skip));
+    res.json({
+      success: true,
+      count: newsCount.length,
+      data: news,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal error occured.",
+    });
+  }
+};
+
+const getNewsById = async (req, res, next) => {
+  try {
+    const news = await News.findById(req.params.newsId).populate({
+      path: "category",
+      select: ["_id", "category_name"],
+    });
+    res.json({
+      success: true,
+      data: news,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal error occured.",
+    });
+  }
+};
+
+const getSliderNews = async (req, res, next) => {
+  try {
+    const news = await News.find({addToSlider:true}).populate({
+      path: "category",
+      select: ["_id", "category_name"],
+    });
+    res.json({
+      success: true,
+      count:news.length,
+      data: news,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal error occured.",
+    });
+  }
+};
+
+const getNewsByCategory = async (req, res, next) => {
+  try {
+    const news = await News.find({category:req.params.catId}).populate({
+      path: "category",
+      select: ["_id", "category_name"],
+    });
+
+    res.json({
+      success: true,
+      count:news.length,
+      data: news,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal error occured.",
+    });
+  }
+};
+const deleteNewsById = async (req, res, next) => {
+  try {
+    const news = await News.findByIdAndDelete(req.params.newsId);
+
+    res.status(201).json({
+      success: true,
+      message: "news deleted successfully",
+      data: news,
+    });
+
+    if (!news) {
+      return res.status(401).json({
+        success: false,
+        message: "news not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal error occured.",
+    });
+  }
+};
+
+
+const editNews = async (req, res, next) => {
+  try {
+    const news = await News.findByIdAndUpdate(
+      req.params.newsId,
+      req.body,
+      {
+        new: true,
+        runValidators:true
+      }
+    );
+    res.status(201).json({
+      success: true,
+      message: "News updated successfully",
+      data: news,
+    });
+
+    if (!news) {
+      return res.status(401).json({
+        success: false,
+        message: "news not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "internal error occured.",
+     });
+  }
+};
 module.exports = {
   addNews,
+  getAllNews,
+  getNewsById,
+  getSliderNews,
+  getNewsByCategory,
+  deleteNewsById,
+  editNews
 };
--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,7 +1,24 @@
-const express=require("express")
-const router= express.Router();
+const express = require("express");
+const router = express.Router();
 const protect = require("../middleware/authMiddleware");
-const { addNews } = require("../controllers/newsController");
+const {
+  addNews,
+  getAllNews,
+  getNewsById,
+  getSliderNews,
+  getNewsByCategory,
+  deleteNewsById,
+  editNews
+} = require("../controllers/newsController");
 
-router.route("/addNews").post(protect,addNews)
-module.exports=router;
+router.route("/addNews").post(protect, addNews);
+router.route("/getAllNews/:pageNo/:pageSize").get(getAllNews);
+// getSliderNews
+
+ router.route("/getNewsById/:newsId").get(getNewsById);
+router.route("/getAllNews/slider").get(getSliderNews);
+router.route("/getNewsByCategory/:catId").get(getNewsByCategory);
+router.route("/deleteNewsById/:newsId").delete(protect,deleteNewsById);
+router.route("/editNews/:newsId").put(protect,editNews);
+
+module.exports = router;
